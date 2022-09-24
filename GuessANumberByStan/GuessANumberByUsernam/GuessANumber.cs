d50d7496461ba886027e145d4f037a2377db2e88
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-09-24 19:43:39 +0300

    GuessANumber Game

---
+++ GuessANumberByStan/GuessANumberByUsernam/GuessANumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GuessANumberByUsernam
+{
+    class GuessANumber
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+
+            Console.WriteLine("Please input your number");
+
+            while (true)
+            {
+                Console.Write("Guess a number between 1 and 100: ");
+                string playersInput = Console.ReadLine();
+                bool isValid = int.TryParse(playersInput, out int playerNumber);
+
+                if (isValid)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it, nice :)");
+                        break;
+                    }
+                    else if (playerNumber>computerNumber)
+                    {
+                        Console.WriteLine("Too High, try lower digit :(");
+
+                    }
+                    else if (playerNumber<computerNumber)
+                    {
+                        Console.WriteLine("Too Low, try higher digit :(");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! This time enter digits...");
+                }
+            }
+        }
+    }
+}
